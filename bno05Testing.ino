1dd81801a7e6c5dc9204a6c86d38627ab9dabe79
Daniel Kouchekinia <kouchekinia@wisc.edu>
2019-09-24 20:30:36 -0500

    Initial commit.

---
+++ bno05Testing.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+
+#include <utility/imumaths.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+void setup() {
+  Serial.begin(9600);
+  bno.begin();
+  bno.setExtCrystalUse(true);
+}
+
+void loop() {
+  sensors_event_t event; 
+  bno.getEvent(&event);
+  
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.print(event.orientation.z, 4);
+  Serial.println("");
+  
+  delay(100);
+
+}
